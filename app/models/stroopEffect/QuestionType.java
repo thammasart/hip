2ca2b95080facb45557d3c5cf9047fd3e6bcad85
InfinitiRb <your_email@example.com>
2014-02-05 21:40:43 +0700

    StroopEffect Model

---
+++ app/models/stroopEffect/QuestionType.java
@@ -0,0 +1,8 @@
+package models.stroopEffect;
+
+/**
+ * Created by keepluck on 4/2/14.
+ */
+enum QuestionType {
+    THAI,ENGLISH,SHAPE
+}
---
+++ app/models/stroopEffect/Quiz.java
@@ -0,0 +1,32 @@
+package models.stroopEffect;
+
+import play.db.ebean.Model;
+
+import javax.persistence.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table (name="stroop_quiz")
+public class Quiz extends Model{
+    @Id
+    public long quiz_id;
+
+    @ManyToOne
+    public Trial trial;
+    @ManyToOne
+    public Question question;
+    @OneToMany
+    public List<Answer> answers;
+
+    public static Quiz create(Trial trial,Question question){
+        Quiz quiz = new Quiz();
+        quiz.trial = trial;
+        quiz.question = question;
+        return quiz;
+    }
+
+    public static Finder<Long,Quiz> find = new Finder(Long.class,Quiz.class);
+
+}
