3121be454ddfebf41a3d73ddbf43a8a540f9bed8
Ratthasorn Thassana <ohmzuu@gmail.com>
2014-02-07 16:44:00 +0700

    admin can edit parameter

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -7,10 +7,13 @@ import views.html.*;
 import models.*;
 import views.html.admin.*;
 import java.util.List;
+import java.util.ArrayList;
+import models.brownPeterson.*;
 
 public class Admin extends Controller{
 
     private static final Form<ExperimentSchedule> expForm = Form.form(ExperimentSchedule.class);
+    private static final Form<Trial> trialForm = Form.form(Trial.class);
 
     public static Result index(){
         User user = User.find.where().eq("username", session().get("username")).findUnique();
@@ -41,7 +44,33 @@ public class Admin extends Controller{
         ExperimentSchedule exp = boundForm.get();
         exp.save();
         flash("success","Successfully");
+        for(int i = 0; i < exp.noOfTrial; i++){
+            Trial.create(exp).save();
+        }
         return redirect(routes.Admin.addExperiment());
     }
 
+    public static Result updateExperiment(long id){
+        Form<ExperimentSchedule> boundForm = expForm.bindFromRequest();
+        ExperimentSchedule exp = ExperimentSchedule.find.byId(id);
+        List<Trial> trials = Trial.findInvolving(exp);
+        if(boundForm.hasErrors()){
+            flash("error", "please correct the form above.");
+            return badRequest(views.html.admin.experiment.edit.render(exp, trials));
+        }
+
+        boundForm.get().update(id);
+        exp = ExperimentSchedule.find.byId(id);
+        flash("success", "update success.");
+        return ok(views.html.admin.experiment.edit.render(exp, trials));
+    }
+
+    public static Result parameter(long id){
+        final ExperimentSchedule exp = ExperimentSchedule.find.byId(id);
+        if(exp == null){
+            return notFound("This Experiment does not exist.");
+        }
+        List<Trial> trials = Trial.findInvolving(exp);
+        return ok(views.html.admin.experiment.edit.render(exp, trials));
+    }
 }
