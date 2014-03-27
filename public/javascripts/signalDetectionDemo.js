37213c1b8ddcfcfd8db08df0c29ff1cc3a1b145c
InfinitiRb <your_email@example.com>
2014-03-27 16:42:57 +0700

    SignalDetection Experiment Fixed

--- public/javascripts/signalDetectionDemo.js
+++ public/javascripts/signalDetectionDemo.js
@@ -6,23 +6,11 @@ var showTimerTime = 0;
 var signalWordTime = 0;
 var questionTime = 0;
 var expDuration = 0;
-var question = "";
-var target;
-var noise;
-var textLength;
-var noOfTarget;
-var displayTime;
+var question = "XXXXXYXXXXXYXXXYXXXXXXXXX";
 
 function showInstruction (){
-    displayTime = document.getElementById("displayTime").innerHTML;
-    displayTime = displayTime*1000;
-    target = document.getElementById("target").innerHTML;
-    noise = document.getElementById("noise").innerHTML;
-    textLength = document.getElementById("length").innerHTML;
-    noOfTarget = document.getElementById("noOfTarget").innerHTML;
     clearInterval(startTimer);
-    generateQuestion();
-    document.getElementById("word").innerHTML=" เป้าหมายคือ " + target;
+    document.getElementById("word").innerHTML=" เป้าหมายคือ Y ";
     showInstructionTime = setInterval(function(){showTimer()},5000);
 }
 function showTimer(){
@@ -35,12 +23,15 @@ function showSignalWord(){
     clearInterval(showTimerTime);
     document.getElementById("word").style.fontSize = "30px";
     document.getElementById("word").innerHTML= question;
-    questionTime = setInterval(function(){showQuestionWord()},displayTime);
+    questionTime = setInterval(function(){showQuestionWord()},5000);
+}
+function showQuestionList(){
+    document.getElementById("word").innerHTML= question;
 }
 function showQuestionWord(){
     clearInterval(questionTime);
     document.getElementById("word").style.fontSize = "60px";
-    document.getElementById("word").innerHTML = " มี "+ target +" อยู่หรือไม่ " ;
+    document.getElementById("word").innerHTML = " มี Y อยู่หรือไม่ " ;
     document.getElementById("true").style.visibility = "visible";
     document.getElementById("false").style.visibility = "visible";
     d = new Date();
@@ -54,7 +45,7 @@ function submitButtonClick(clicked_name){
         document.getElementById("answer").checked= true;
      else
         document.getElementById("answer").checked = false;
-     if ( (noOfTarget > 0 && clicked_name == "true") || (noOfTarget == 0 && clicked_name == "false") )
+     if (clicked_name == "true")
         document.getElementById("isCorrect").checked = true;
      else
         document.getElementById("isCorrect").checked = false;
@@ -62,31 +53,4 @@ function submitButtonClick(clicked_name){
 function calculateTimeLeft(){
     count=count-1;
     document.getElementById("word").innerHTML= " โจทย์จะเริ่มในอีก " + (count-1) +" วินาที " ;
-}
-function generateQuestion(){
-    var targetChar = new Array();
-
-    for (var i = 0;i<noOfTarget;i++){
-        var isUnique = false;
-        var randIndex =1;
-        do{
-            isUnique = true;
-            randIndex = Math.floor(Math.random()*textLength);
-            for (var j = 0;j<targetChar.length;j++)
-                if (randIndex == targetChar[j])
-                    isUnique = false;
-        }while(!isUnique);
-        targetChar[i] = randIndex;
-    }
-
-    for (var i = 0;i<textLength;i++){
-        var isTarget = false;
-        for (var j = 0;j<noOfTarget;j++)
-            if (targetChar[j] == i)
-                isTarget = true;
-        if (isTarget)
-            question += target;
-        else
-            question += noise;
-    }
 }
