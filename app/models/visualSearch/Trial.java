85fde87be021c4e0bdda6ee02d21cd9c435a9c7a
sakgunner <sakgunner@gmail.com>
2014-05-09 17:07:38 +0700

    add trial in model visual search

---
+++ app/models/visualSearch/Trial.java
@@ -0,0 +1,25 @@
+package models.visualSearch;
+
+import models.ExperimentSchedule;
+
+import play.db.ebean.Model;
+import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table (name="visual_search_trial")
+public class Trial extends Model{
+    @Id
+    public long id;
+    @ManyToOne
+    public ExperimentSchedule schedule;
+
+    public Trial(ExperimentSchedule schedule){
+    	this.schedule = schedule;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static Finder<Long, Trial> find = new Finder(Long.class, Trial.class);
+
+}
---
+++ test/models/unitTest/VisualSearchUnitTest.java
@@ -0,0 +1,37 @@
+package models.unitTest;
+
+import models.visualSearch.*;
+import models.ExperimentSchedule;
+import models.ExperimentType;
+import models.User;
+import play.test.WithApplication;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static play.test.Helpers.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
+
+public class VisualSearchUnitTest extends WithApplication {
+
+    @Before
+    public void setUp(){
+        start(fakeApplication(inMemoryDatabase(), fakeGlobal()));
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, -1);
+        Date lastYearDate = calendar.getTime();
+        calendar.add(Calendar.YEAR, +2);
+        Date nextYearDate = calendar.getTime();
+        new ExperimentSchedule("Visual Search Experiment", 3, lastYearDate, nextYearDate, ExperimentType.VISUALSEARCH).save();
+        new User("test","test").save();
+    }
+
+    @Test
+    public void createTrialShouldNotNull(){
+        ExperimentSchedule exp = ExperimentSchedule.find.byId(1L);
+        Trial trial = new Trial(exp);
+        assertNotNull(trial);
+        assertEquals(exp, trial.schedule);
+    }
+}
