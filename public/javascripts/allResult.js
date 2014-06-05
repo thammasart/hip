5b54a88401c04051d9d9012b26c8bd847a822f56
peat2110 <pppppooo@hotmail.com>
2014-06-05 19:41:47 +0700

    overlay

--- public/javascripts/allResult.js
+++ public/javascripts/allResult.js
@@ -5,3 +5,8 @@
  function closeOverlay() { 
     document.getElementById("overLay").style.visibility = "hidden";  
  } 
+
+$('#myTab a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
