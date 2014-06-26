38ba6b305a95f539220144bcbd74a685aa205830
peat2110 <pppppooo@hotmail.com>
2014-06-26 17:35:28 +0700

    wait for model

--- public/javascripts/allResult.js
+++ public/javascripts/allResult.js
@@ -6,6 +6,15 @@
     document.getElementById("overLay").style.visibility = "hidden";  
  } 
 
+ function selectExperiment(expName) { 
+    document.getElementById("expName").innerHTML = expName +' <span class="caret"></span>';
+    document.getElementById("overlayHeader").innerHTML = expName;
+    
+    for (var i=0;i<document.getElementsByName('gearButton').length;i++){
+        document.getElementsByName('gearButton')[i].disabled=false;
+        document.getElementsByName('gearButton')[i].style.backgroundColor = "white";
+    }
+ } 
 $('#myTab a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
