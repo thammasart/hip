974d8d1be9f51cd29427cbcc7b8a8e40e35b5831
peat2110 <pppppooo@hotmail.com>
2014-07-03 21:16:16 +0700

    allresult tab

---
+++ app/models/AllResult.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class AllResult{
+
+    public String startDate;
+    public String expireDate;
+    public String scheduleName;
+    public long trialId;
+    
+}
