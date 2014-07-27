88b14d9b2861843ae6adb80e2f71385e55e1686a
Ratthasorn Thassana <ohmzuu@gmail.com>
2014-07-27 14:20:39 +0700

    fix bug display shape correct

--- public/javascripts/controllers/visualsearch.js
+++ public/javascripts/controllers/visualsearch.js
@@ -11,6 +11,8 @@ angular.module('VisualSearchApp',[])
             return colorText;
         }
 
+
+
         $scope.init = function(trialId){
             console.log('init');
             $http({method:'GET',url:'init',params:{trialId:trialId}}).success(function(result){
@@ -18,9 +20,42 @@ angular.module('VisualSearchApp',[])
                 console.log($scope.trial);
                 console.log($scope.sharps);
                 $scope.sharps = angular.fromJson($scope.trial.quiz.question.sharps);
+                $scope.target = generateTarget();
+                console.log($scope.sharps);
                 
             }).error(function(result){
                 console.log('error:' + result);
             });
+        }
+
+        function generateTarget(){
+            var obj = {};
+            obj.top = $scope.trial.quiz.positionYofTarget;
+            obj.left = $scope.trial.quiz.positionXofTarget;
+            if($scope.trial.quiz.target == 'CIRCLE_RED'){
+                obj.color = 'red';
+                obj.sharp = 'circle';
+            }
+            else if($scope.trial.quiz.target == 'CIRCLE_GREEN'){
+                obj.color = 'green';
+                obj.sharp = 'circle';
+            }
+            else if($scope.trial.quiz.target == 'SQAURE_BLUE'){
+                obj.color = 'blue';
+                obj.sharp = 'square';
+            }
+            else if($scope.trial.quiz.target == 'SQAURE_RED'){
+                obj.color = 'red';
+                obj.sharp = 'square';
+            }
+            else if($scope.trial.quiz.target == 'SQAURE_GREEN'){
+                obj.color = 'green';
+                obj.sharp = 'square';
+            }
+            else if($scope.trial.quiz.target == 'CIRCLE_BLUE'){
+                obj.color = 'blue';
+                obj.sharp = 'circle';
+            }
+            return obj;
         }
     }]);
