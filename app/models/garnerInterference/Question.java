51d31422aafdb96eb3e43dcb9807e57ea11aded4
Ratthasorn Thassana <ohmzuu@gmail.com>
2014-08-01 23:02:11 +0700

    add new logic for generate garner question

--- app/models/garnerInterference/Question.java
+++ app/models/garnerInterference/Question.java
@@ -2,6 +2,7 @@ package models.garnerInterference;
 
 import play.db.ebean.Model;
 import javax.persistence.*;
+import java.util.Random;
 
 @Entity
 @Table (name="garner_interference_question")
@@ -14,7 +15,10 @@ public class Question extends Model{
     public String sizeQuestion;
     public boolean colorMatch;
     public boolean sizeMatch;
-    
+
+    public static final String[] SIZES = {"big", "small"};
+    public static final String[] COLORS = {"dark", "light"};
+
     public Question(){
     	
     }
@@ -33,25 +37,45 @@ public class Question extends Model{
     }
 
     private static void createSimpleBiDirectionQuestion(Question question, boolean isNotFake) {
-        question.sizePic = "big";
-        question.sizeQuestion = isNotFake ? "big" : "small";
+        question.sizePic = generateSizePic();
+        question.sizeQuestion = isNotFake ? question.sizePic : calculateSizeFake(question.sizePic);
         question.sizeMatch = isNotFake ? true : false;
-        question.colorPic = "dark";
+        question.colorPic = generateColorPic();
+        question.colorQuestion = isNotFake ? question.colorPic : calculateColorFake(question.colorPic);
+        question.colorMatch = isNotFake ? true : false;
     }
 
+    private static String calculateSizeFake(String sizePic) {
+        if(sizePic.equals(SIZES[0]))
+            return SIZES[1];
+        return SIZES[0];
+    }
+
+
+
     private static void createSimpleSizeQuestion(Question question, boolean isNotFake) {
-        question.sizePic = "big";
-        question.sizeQuestion = isNotFake ? "big" : "small";
+        question.sizePic = generateSizePic();
+        question.sizeQuestion = isNotFake ? question.sizePic : calculateSizeFake(question.sizePic);
         question.sizeMatch = isNotFake ? true : false;
-        question.colorPic = "dark";
-        question.colorQuestion = isNotFake ? "dark" : "light";
-        question.colorMatch = isNotFake ? true : false;
+        question.colorPic = generateColorPic();
+    }
+
+    private static String calculateColorFake(String colorPic) {
+        return colorPic.equals(COLORS[0]) ? COLORS[1] : COLORS[0];
+    }
+
+    private static String generateColorPic() {
+        return COLORS[new Random().nextInt(COLORS.length)];
+    }
+
+    private static String generateSizePic() {
+        return SIZES[new Random().nextInt(SIZES.length)];
     }
 
     private static void createSimpleColorQuestion(Question question,boolean isNotFake) {
-        question.colorPic = "dark";
-        question.colorQuestion = isNotFake ? "dark" : "light";
+        question.colorPic = generateColorPic();
+        question.colorQuestion = isNotFake ? question.colorPic : calculateColorFake(question.colorPic);
         question.colorMatch = isNotFake ? true : false;
-        question.sizePic = "big";
+        question.sizePic = generateSizePic();
     }
 }
